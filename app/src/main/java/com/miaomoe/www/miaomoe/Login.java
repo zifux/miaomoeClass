eee7b44f524d936bddf2dad7ccb674a92decbcd2
xiuu@qq.com <xiuu@qq.com>
2015-03-03 09:52:07 +0800

    增加两个菜单项

--- app/src/main/java/com/miaomoe/www/miaomoe/Login.java
+++ app/src/main/java/com/miaomoe/www/miaomoe/Login.java
@@ -6,18 +6,20 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 
 public class Login extends ActionBarActivity implements View.OnClickListener {
     private android.support.v7.app.ActionBar abr;
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         abr=getSupportActionBar();
         abr.setSubtitle("\t 教务处学生园地\\(≧ω≦)");
         Resources res=getResources();
         abr.setBackgroundDrawable(res.getDrawable(R.drawable.bar2));
+        Log.i("y8ijg","dfdddddddddddddddddddddddddddddddddd");
         setContentView(R.layout.login);
     }
     @Override
