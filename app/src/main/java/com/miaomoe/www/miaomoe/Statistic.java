d2a2dd5878cc962ff98aa3515c47587cbe7626f2
zifux <xiuu@qq.com>
2015-03-24 23:12:54 +0900

    commit

--- app/src/main/java/com/miaomoe/www/miaomoe/Statistic.java
+++ app/src/main/java/com/miaomoe/www/miaomoe/Statistic.java
@@ -1,7 +1,32 @@
 package com.miaomoe.www.miaomoe;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
 /**
- * Created by xiuu on 2015/3/24 0024.
+ * Created by xiuu on 2015/2/25 0025.
  */
-public class Statistic {
+public class Statistic extends ActionBarActivity implements View.OnClickListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.findempty);
+        TextView a= (TextView) findViewById(R.id.emptytextView);
+        a.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.i("s","s");
+        setResult(200);
+        finish();
+    }
 }
